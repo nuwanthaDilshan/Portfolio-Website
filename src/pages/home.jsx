5b0097afa8132ff3db9c7190eb441f1ee4df2831
nuwanthaDilshan <nuwanthadilshan56@gmail.com>
2025-02-02 18:03:01 +0530

    😁 small update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <MouseBall />
         <div className="bg-[#062239] min-h-screen">
           <main className="pt-18 p-3 max-w-7xl mx-auto text-gray-700">
-            <section id="home">Home Sectionlo</section>
+            <section id="home">Home Section</section>
           </main>
         </div>
       </>
